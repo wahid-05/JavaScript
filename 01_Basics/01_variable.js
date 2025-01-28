934795477b183fafb83853865312dbd302d0aa8c
MohdGous Shakeel Kurne <143505992+wahid-05@users.noreply.github.com>
2025-01-28 07:33:00 +0000

    Discussion on variables

---
+++ 01_Basics/01_variable.js
@@ -0,0 +1,18 @@
+const accountId = 52040
+let accountEmail = "wskurne05@gmail.com"
+var accountPassword = "Campus@2023"
+accountCity = "Pune"
+let accountState;
+
+// accountId = 2 //not Allowed
+
+accountEmail = "hdfc@.com"
+accountPassword = "2341"
+accountCity = "Bangal"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+// console.log(accountId)
+/*
+Prefer not to use var
+Because of issues in block scope and functional scope
+ */
